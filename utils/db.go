e59906300b6a5af033196e91ec8d4f959916babc
wxr_624@163.com <Wxr0910+1s>
2022-04-04 05:47:40 +0800

    study sql

--- utils/db.go
+++ utils/db.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	DB  *sql.DB
+	err error
+)
+
+func init() {
+	DB, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/test")
+	if err != nil {
+		panic(err.Error())
+	}
+}
