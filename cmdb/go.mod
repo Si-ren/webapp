a7b871792bd6f2197c340e21fedd17469d0b8bd6
Sirius <wxr_624@163.com>
2023-01-04 03:15:08 +0800

    update

--- cmdb/go.mod
+++ cmdb/go.mod
@@ -21,6 +21,18 @@ require (
 )
 
 require (
+	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.4 // indirect
+	github.com/alibabacloud-go/darabonba-openapi/v2 v2.0.2 // indirect
+	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
+	github.com/alibabacloud-go/ecs20140526-20140526/v3 v3.0.1 // indirect
+	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
+	github.com/alibabacloud-go/openapi-util v0.0.11 // indirect
+	github.com/alibabacloud-go/tea v1.1.19 // indirect
+	github.com/alibabacloud-go/tea-utils v1.3.1 // indirect
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.0 // indirect
+	github.com/alibabacloud-go/tea-xml v1.1.2 // indirect
+	github.com/aliyun/credentials-go v1.1.2 // indirect
+	github.com/clbanning/mxj/v2 v2.5.5 // indirect
 	github.com/favadi/protoc-go-inject-tag v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -38,6 +50,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tjfoc/gmsm v1.3.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/net v0.1.0 // indirect
--- cmdb/provider/aliyun/connectivity/client.go
+++ cmdb/provider/aliyun/connectivity/client.go
@@ -1,26 +1,37 @@
 package connectivity
 
-import "github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+import (
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	ecs20140526 "github.com/alibabacloud-go/ecs-20140526/v3/client"
+	"github.com/alibabacloud-go/tea/tea"
+)
 
-// NewAliCloudClient client
-func NewAliCloudClient(ak, sk, region string) *AliCloudClient {
-	return &AliCloudClient{
-		Region:       region,
-		AccessKey:    ak,
-		AccessSecret: sk,
+func NewClient(accessKeyId *string, accessKeySecret *string, region *string) (client *ecs20140526.Client, _err error) {
+	config := &openapi.Config{
+		//Region
+		RegionId: region,
+		// 必填，您的 AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 必填，您的 AccessKey Secret
+		AccessKeySecret: accessKeySecret,
 	}
+	// 访问的域名
+	config.Endpoint = tea.String("ecs20140526.cn-shanghai.aliyuncs.com")
+	client = &ecs20140526.Client{}
+	client, _err = ecs20140526.NewClient(config)
+	return client, _err
 }
 
 type AliCloudClient struct {
 	Region       string
 	AccessKey    string
 	AccessSecret string
-	ecsConn      *ecs.Client
+	ecsConn      *ecs20140526.Client
 }
 
 // EcsClient 客户端
-func (c *AliCloudClient) EcsClient() (*ecs.Client, error) {
-	client, err := ecs.NewClientWithAccessKey(c.Region, c.AccessKey, c.AccessSecret)
+func (c *AliCloudClient) EcsClient() (*ecs20140526.Client, error) {
+	client, err := ecs20140526.NewClient(&(c.AccessKey), &(c.AccessSecret), &(c.Region))
 	if err != nil {
 		return nil, err
 	}
